9ead00b9243548c9b433840e52d7cbbdd3356d7e
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-04-27 14:58:06 +0000

    conversion operations

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let abhishek = Number(score)
+console.log(typeof abhishek);
+console.log(abhishek);
+
+/*
+33 = 33
+"33abc" = nan
+true = 1
+false = 1
+*/
+
+// boolean 0,1 value to true, flase
+
+let IsloggedIn = 0
+let BooleanIsloggedIn = Boolean(IsloggedIn) 
+console.log(BooleanIsloggedIn);
