c50371918ea91bded1102dc6427076bfb8d60ce8
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-04-27 12:43:49 +0000

    datatypes and variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1409
+let accountEmail = "abhishek149@gmail.com"
+let accountpassword = "12345"
+let accountcity = "Jaipur"
+let accountstate;
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate]);
+
+/* 
+const can not be change
+let can change
+not use var because of error of block scope {}
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" ; //for newer version
+
+//alert ( 3 + 3 ) we are using node.js , not browseer
+
+console.log(3+3);
+console.log("abhishek");
+
+let age = 18
+let name = "abhishek"
+let IsloggedIn = false
+let state = ""
+
+
+/*
+datatypes :
+number = 123
+boolean = true false
+string = ""
+null
+undefined = not defined like state
+symbol = unique
+object
+*/
+
+console.log(typeof null); // null's type is object
+console.log(typeof undefined); //it's undefined
+console.log(state);
+
+
+
