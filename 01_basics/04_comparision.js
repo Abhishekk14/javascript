5e8f4c2054215e27ebdd6db742a3eaefad80fdff
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-04-27 18:21:04 +0000

    Comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+//types of comparisons 
+// console.log(2 > 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+
+//
+// console.log("2">1); convert automatically and give result
+// console.log("02">1);
+
+// console.log(null == 0); false
+// console.log(null > 0); false
+// console.log(null >= 0); true don't know why
+
+// console.log(undefined == 0); always 00 beacuse not defined
+// console.log(undefined >= 0);
+
+//console.log("2"===2);  it is strict equal , also read data types not only number
+  
+
+// always do clean coding
+
+
+
+
