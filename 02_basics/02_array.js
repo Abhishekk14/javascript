456765d1fa4a110e42949b44561bf624b56534ba
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-05-02 06:33:58 +0000

    array in depth

---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+const marvel_heros = ["ak", "dk" , "dl"]
+const dc_heros = ["jk", "pk" , "mc"]
+
+// marvel_heros.push(dc_heros) ;
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const All = marvel_heros.concat(dc_heros)
+
+// console.log(All);
+
+// const all2 = [...marvel_heros,...dc_heros]
+// console.log(all2);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+// console.log(Array.isArray("abhi"));
+// console.log(Array.from("abhi"));
+// console.log(Array.from({name : "abhi"})); //out of mind
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
