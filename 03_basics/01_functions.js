92799c79797ebdd4f8590f56f2567b7e65832fc1
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-05-03 05:05:18 +0000

    Function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,29 @@
+
+// function saymyname (){
+//     console.log("a");
+//     console.log("b");
+//     console.log("c");
+//     console.log("d");    
+// }
+
+// saymyname()
+
+// function addtwonum(num1 , num2){
+//     // let result = num1 + num2
+//     // console.log("abhi");
+//     // return result  
+//     return  num1+num2
+// }
+// const result = addtwonum(14,"24")
+
+// console.log("Result = " , result);
+
+// function loginusermsg (username){
+//     return `${username} just logged in`
+// }
+// // const user = loginusermsg("abhi")
+
+// //  console.log(user);
+
+// console.log(loginusermsg("abhi"));
+
