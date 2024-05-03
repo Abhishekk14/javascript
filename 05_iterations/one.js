8c01f6ab8a2df0310a5b8c5cabb0199226e91e80
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-05-03 17:01:44 +0000

    for

---
+++ 05_iterations/one.js
@@ -0,0 +1,47 @@
+// for
+// for (let i = 0; i < 10; i++) {
+//     const element = i ;
+//     if (element===5) {
+//         console.log("5 is best");
+//     }
+//     console.log(element);
+    
+// }
+
+for (let i = 0; i < 10; i++) {
+    // console.log(`outer loop value : ${i}`);
+    for (let j = 0; j < 10 ; j++) {
+        // console.log(`inner loop value : ${j}`);
+        // console.log(`${i} * ${j} = ${i*j}` );
+    }
+    
+    
+}
+
+// let myarr = [ "abhi" , "dk" , "pk"]
+
+// for (let index = 0; index < myarr.length; index++) {
+//     const element = myarr[index];
+//     console.log(element);
+    
+// }
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
